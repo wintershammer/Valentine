c19bd54c5441d7997217f392e322ca5c89fb3e8e
wintershammer <wintershammer@gmail.com>
2015-04-02 15:57:48 +0300

    Added basic file loading and repl functionality
    Driver now can either load a file to interpret or start a rudimentary
    REPL

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,8 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
-
+import java.io.InputStreamReader;
 import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
@@ -9,14 +10,43 @@ public class Driver {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		
-		MyGLexer lexer = new MyGLexer(new ANTLRFileStream(args[0]));
-		MyGParser parser = new MyGParser(new CommonTokenStream(lexer));
-		ParseTree tree = parser.program();
-		MyVisitor visitor = new MyVisitor();
-		visitor.visit(tree);
-		
+
+		if (!args[0].equalsIgnoreCase("repl")) {
+			MyGLexer lexer = new MyGLexer(new ANTLRFileStream(args[0]));
+			MyGParser parser = new MyGParser(new CommonTokenStream(lexer));
+			ParseTree tree = parser.program();
+			MyVisitor visitor = new MyVisitor();
+			visitor.visit(tree);
+			
+		} else if (args[0].equalsIgnoreCase("repl")) { //THIS NEEDS SOME CLEANUP AND A WAY TO DELETE PREVIOUS LINES
+			MyVisitor visitor = new MyVisitor(); //you want this outside the loop so it retains its enviroment/memory!
+			try {
+				boolean run = true;
+				while(run){
+					System.out.print(">>>");
+					BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+					String input;
+					StringBuffer sb = new StringBuffer();
+					while ((input = br.readLine()).length()!=0) {
+						sb.append(input);
+					}
+					if(sb.toString().equals("exit")){
+						run = false;
+					}
+					MyGLexer lexer = new MyGLexer(new ANTLRInputStream(sb.toString()));
+					MyGParser parser = new MyGParser(new CommonTokenStream(lexer));
+					ParseTree tree = parser.program();
+					visitor.visit(tree);
+				}
+			} 
+			catch (IOException io) {
+				io.printStackTrace();
+			}
+			
+		} 
+		else {
+			System.out.println("Invalid argument");
+		}
 
 	}
-
 }
